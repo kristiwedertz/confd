776fc79f560688b8abaf9e84a265e7e75925c7ca
Kristi Wedertz <kristiwedertz@gmail.com>
2019-01-25 21:02:31 -0800

    update aws-creds script

--- aws-creds.sh
+++ aws-creds.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 FILE='./credentials'
-echo '[default]' >> $FILE
-echo 'aws_access_key_id = ' >> $FILE
-echo 'aws_secret_access_key_id = '>> $FILE
-echo 'region = ' >> $FILE
-sed -i "/aws_access_key_id = / s/$/$AWS_ACCESS_KEY_ID/" $FILE
-sed -i "/aws_secret_access_key_id = / s/$/$AWS_SECRET_ACCESS_KEY/" $FILE
-sed -i "/region = / s/$/$AWS_REGION/" $FILE
+echo "[default]" >> $FILE
+echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> $FILE
+echo "aws_secret_access_key_id = $AWS_SECRET_ACCESS_KEY" >> $FILE
+echo "region = $AWS_REGION" >> $FILE
 mkdir ~/.aws/
 mv ./credentials ~/.aws/credentials
